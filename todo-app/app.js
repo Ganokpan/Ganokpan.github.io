349cdf160aa4250ddd33afc320f140433493685b
Ganokpan <gloy12257@gmail.com>
2025-06-29 16:00:22 +0700

    Add index and style

---
+++ todo-app/app.js
@@ -0,0 +1,26 @@
+c
+
+//เพิ่ม todo
+addTodo = () =>{
+
+}
+
+//สร้าง List รายการ
+createTodoItem = () =>{
+
+}
+
+//update รายการ
+updateTodoItem = () =>{
+
+}
+
+//ลบรายการ
+DeleteTodoItem = () =>{
+
+}
+
+//นับจำนวนรายการที่ยังไม่ได้ทำ
+countTodoItem = () =>{
+
+}
